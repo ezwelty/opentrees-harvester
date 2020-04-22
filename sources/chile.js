51816095b153c526e8cb5e10e95c85790945fe4d
ezwelty <ethan.welty@gmail.com>
2020-04-22 12:45:05 -0600

    Update Providencia, Chile with crosswalk and url

--- sources/chile.js
+++ sources/chile.js
@@ -11,13 +11,20 @@ module.exports = [
     centre: { lon: -70.877, lat: -29.859 }
   },
   {
-    pending: true,
     id: 'providencia',
     country: 'Chile',
     short: 'Providencia',
     long: 'Commune de Providencia',
-    download: 'http://datos.providencia.cl/datastreams/92199-catrastro-de-arboles-en-la-comuna-de-providencia.csv',
+    download: 'http://datos.gob.cl/datastore/dump/c4a710d5-c221-4da6-8a91-c0d8c9500164',
     info: 'http://datos.gob.cl/dataset/catrastro_de_arboles_en_la_comuna_de_providencia/resource/c4a710d5-c221-4da6-8a91-c0d8c9500164',
-    srs: 'EPSG:32719', // guessed by WhatTheProj
-  },
-].map(s => ({ ...s, country: 'Chile' }))
+    srs: 'EPSG:32719',
+    license: { id: 'PDDL-1.0' },
+    crosswalk: {
+      ref: '_id',
+      scientific: 'NOMB_CIENT',
+      common: 'SP',
+      height: 'H_MTS'
+      // TODO: FECHA = updated ?
+    }
+  }
+].map(s => ({ ...s, country: 'Chile', language: 'es-CL' }))
